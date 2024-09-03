fe18e89775a7b8c82f75f62ad0ec3b6f9fad1d71
weizi.chen <weizi.chen@kone.com>
2024-09-03 16:08:11 +0800

    fix: update peco

--- .config/fish/functions/peco_select_history.fish
+++ .config/fish/functions/peco_select_history.fish
@@ -1,8 +1,8 @@
 function peco_select_history
     if test (count $argv) = 0
-        set peco_flags --layout=bottom-up
+        set peco_flags --layout=top-down
     else
-        set peco_flags --layout=bottom-up --query "$argv"
+        set peco_flags --layout=top-down --query "$argv"
     end
 
     history | peco $peco_flags | read foo
